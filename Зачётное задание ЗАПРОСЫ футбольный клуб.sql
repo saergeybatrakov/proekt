8daff75ba1d740eaf2574d2727cdd33b0ab3d7a0
saergeybatrakov <zenit0259@gmail.com>
2025-03-04 16:11:19 +0700

    Зачётное задание запросы футбольный клуб

---
+++ Зачётное задание ЗАПРОСЫ футбольный клуб.sql
@@ -0,0 +1,5 @@
+SELECT * FROM mydb.`football club` WHERE name='UfaF';
+
+SELECT * FROM mydb.`sponsor` WHERE Name_Sponsor='Bereza';
+
+SELECT * FROM mydb.`football club` JOIN mydb.player WHERE Name_Player='Bogdan';
